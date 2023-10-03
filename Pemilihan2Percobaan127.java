8121903e12cc781828df073aeb589280e66504f9
SuryaRf <141994348+SuryaRf@users.noreply.github.com>
2023-10-03 11:54:21 +0700

    Update Pemilihan2Percobaan127.java

--- Pemilihan2Percobaan127.java
+++ Pemilihan2Percobaan127.java
@@ -4,6 +4,7 @@ public class Pemilihan2Percobaan127 {
 
     public static void main(String[] args) {
         Scanner input27 = new Scanner(System.in);
+        System.out.print("Masukan Tahun : ");
         int tahun = input27.nextInt();
 
 
